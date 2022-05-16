04f02404af1829972a241bca21584bbd67792d44
henrique brun <hpabrun@gmail.com>
2022-05-15 21:19:48 -0300

    subindo exer  1251

--- AvaliacaoSprint02/DigameAFrequencia.cs
+++ AvaliacaoSprint02/DigameAFrequencia.cs
@@ -13,6 +13,39 @@ namespace AvaliacaoSprint02
         {
             //-EXE-1251-DIGA-ME A FREQUENCIA-------------------------------------------------  
 
+
+            string entrada;
+            StringBuilder magia = new StringBuilder();
+
+            while (!string.IsNullOrWhiteSpace(entrada = Console.ReadLine()))
+            {
+
+
+
+
+                byte[] arrayASCII = Encoding.ASCII.GetBytes(entrada);
+
+
+                var listaASCII = arrayASCII.GroupBy(x => x);
+
+
+                var listaOrdemASCII = listaASCII.OrderBy(x => x.Count()).ThenByDescending(x => x.Key);
+
+
+
+                foreach (var simbolo in listaOrdemASCII)
+                {
+                    magia.AppendLine($"{simbolo.Key} {simbolo.Count()}");
+                }
+                magia.AppendLine();
+
+
+            }
+
+            magia.Remove(magia.Length - 1, 1);
+
+            Console.Write(magia);
+
         }
     }
 }
